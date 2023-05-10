1c87c1361749efa920da75a79f2016b7635d8b4f
WillStevens <will.stevens@srm.org.uk>
2023-05-11 00:45:31 +0100

    Update parsing3.asm

--- experiments/parsing3.asm
+++ experiments/parsing3.asm
@@ -1,4 +1,3 @@
-; Another attempt at a better tokenizer
 ; Idea behind this parser is that no input
 ; buffer is needed - area after program
 ; is used to parse tokens as they are
@@ -26,7 +25,12 @@ GetLine:
 
 
 NextCharLoop:
-  CALL GetChar
+
+	IN 1
+	ANI 1
+	JZ NextCharLoop
+	IN 0
+  
 	PUSH PSW
 	
   ; Do we have the same class as before?
@@ -34,7 +38,7 @@ NextCharLoop:
   CALL CompareHLDE
   ; Z,C if it is the same class, NZ,NC otherwise
 
-  PCHL ; Jump based on previous CharClass pointer (will jump to NoCharClass if not set)
+  PCHL ; Jump based on previous CharClass pointer 
 
 NoCharClass:
   XCHG ; Put new class into HL
@@ -46,7 +50,7 @@ DigitClass:
 
   POP PSW
  
-  ; Accumulate the value
+  ; Accumulate the value into B
 	
 	; Muliply by 10
 	XCHG 	; preserve HL in DE
@@ -77,10 +81,11 @@ DigitClassEnd:
   ; need to preserve DE, don't care about HL
   
   LHLD PROG_PARSE_PTR
-  MVI M,1 ; IntegerToken
+  MVI M,27 ; IntegerToken
   INX H
   MOV M,C
   INX H
+WriteToken:
   MOV M,B
   INX H
   SHLD PROG_PARSE_PTR
@@ -94,23 +99,81 @@ CompClass:
 LT0Class:
 	JNZ TokenClassEnd
 	
-	; TODO write the char into the buffer
-	
-	INX B ; increase count of number of chara
-	
 	POP PSW
+	
+	XCHG
+	LHLD PROG_PARSE_PTR
+	DAD B
+	MOV M,A
+	XCHG
+	
+	INX B ; increase count of number of characters
+	
 	JMP NextCharLoop
 
 AlphaClassEnd:
-	; If C=1 then it's a var
-	MOV A,C
-	CPI 1
+	; If BC=1 then it's a var
+	DCX B
 	JZ VarToken
 	
 	; otherwise fall through and look up the token
 
 TokenClassEnd:
 	; Need to preserve DE, don't care about HL
+	PUSH D
+	LHLD PROG_PARSE_PTR
+	; set the high bit of the last char 
+	DAD B
+	MOV A,M
+	ORI 080h
+	MOV M,A
+	
+	XCHG ; put start of token into D
+	LXI H,TokenList
+	
+LookupToken:
+
+	MOV B,M ; B contains the token value
+					; if we get a match
+	
+	PUSH D
+	CM StrcmpEntry
+	POP D
+
+	JZ FoundToken
+
+	MOV A,M
+	INR A
+	INX H
+  JNZ LookupToken
+	CALL Error
+	
+FoundToken:
+	POP D
+	LHLD PROG_PARSE_PTR
+	JMP WriteToken
+	
+StrcmpEntry:
+	INX H
+	
+; DE points to hi-bit terminated string
+; HL points to hi-bit terminated string
+; Returns Z set if match
+; Can't have 128 as a character in D
+; (so assume that strings must be ASCII)
+Strcmp: ; Depth = 3
+	LDAX D
+	CMP M
+	RNZ	; On return Z will be clear
+	
+	CMA
+	ANI 128
+	RZ	; On return Z will be set 
+	
+	INX D
+	INX H
+	JMP Strcmp
+	
 	JMP NoCharClass
 
 VarToken:
@@ -163,3 +226,16 @@ CompareHLDE:
 	RNZ
 	STC
 	RET
+
+Error:
+	JMP Error
+	
+org 0300h
+
+TokenList:
+	DB PrintSub&0ffh
+	DB "PRIN",'T'+128
+	DB 255
+	
+PrintSub:
+	NOP
