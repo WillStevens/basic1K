daf8e31424c3c48c0049ea5e48936facc854191a
WillStevens <will.stevens@srm.org.uk>
2023-05-14 00:29:14 +0100

    Update parsing3.asm
    Commit prior to initial debug

--- experiments/parsing3.asm
+++ experiments/parsing3.asm
@@ -8,14 +8,12 @@
 ; HL contains the address of subroutine to call
 ; for current char class 
 ;
-; BC is used for parse state
+; DE is used for parse state
 
 org 0400h
 
 PROG_PARSE_PTR:
 	DW PROG_BASE
-PARSE_STATE:
-	DW 0
 PROG_BASE:
 
 org 0000h
@@ -28,8 +26,7 @@ GetLine:
 
 FreshStart:
 
-  LXI H,NoCharClass
-
+  LXI H,NoCharClass&0ffh
 
 NextCharLoop:
 
@@ -38,13 +35,13 @@ NextCharLoop:
 	JZ NextCharLoop
 	IN 0
 	
-	MOV D,A
+	MOV B,A
 	
   ; Do we have the same class as before?
   CALL CharClass
-  ; are L and E equal?
+  ; are L and C equal?
   MOV A,L
-  XRA E
+  XRA C
   ; Z if they are equal, NZ if not
   
   PCHL ; Jump based on previous CharClass pointer 
@@ -52,28 +49,31 @@ NextCharLoop:
 DigitClass:
   JNZ DigitClassEnd
  
-  ; Accumulate the value into B
-	XCHG ; presevre HL
+ 	PUSH H
+  ; A is zero at this point
+  
+  ; Accumulate the value into D
+
 	; Muliply by 10
-	MOV H,B
-	MOV L,C
+	MOV H,D
+	MOV L,E
 	
 	DAD H
 	DAD H
-	DAD B
+	DAD D
 	DAD H
 	
 	; Add in the new digit
+	
+	MOV D,A
+	MOV A,B
 	ANI 0fh
-
-	MVI B,0
-	MOV C,D
-	DAD B
+	MOV E,A
+	DAD D
 	
-	MOV B,H
-	MOV C,L
+	XCHG
 	
-	XCHG ; get back HL
+	POP H
 	
   JMP NextCharLoop
 
@@ -84,18 +84,18 @@ DigitClassEnd:
   XTHL
   MVI M,28
   INX H
-  MOV M,C
+  MOV M,E
   INX H
 Write_Shared:
-  MOV M,B
+  MOV M,D
   INX H
   XTHL
 
 NoCharClass:
-  MOV L,E
+  MOV L,C
   XRA A ; set Z
-  MOV B,A ; reset state information
-  MOV C,A
+  MOV D,A ; reset state information
+  MOV E,A
 
   PCHL
   
@@ -105,10 +105,10 @@ QuoteClassExpEnd:
 	
 	POP H
 	PUSH H ; preserve original value
-	DAD B
-	MOV M,C ; put in -ve length
+	DAD D
+	MOV M,E ; put in -ve length
 	
-	DCX B
+	DCX D
 
 	XTHL
 	
@@ -124,10 +124,10 @@ QuoteClass:
 	XTHL
 	MVI M,27
 	INX H
-	MOV M,D
+	MOV M,B
 	XTHL
 	
-	MVI L,QuoteClassExpEnd&0ffh
+	LXI H,QuoteClassExpEnd
 	
 	; will be 3 bytes 
 	CPI (QuoteClass^QuoteClassExpEnd) & 0ffh
@@ -146,111 +146,137 @@ CompClass:
 AlphaClass:
 	
 	XTHL
-	PUSH H
-	DAD B
-	MOV M,D
-	POP H
+	MOV M,B
+	INX H
 	XTHL
+	
+	DCX D ; increase char count
 	
 	; if NZ then we will just
 	; have written a different class char:
 	; good, this ensures no spurious
 	; strcmp matches
-	JNZ TokenClassEnd 
 	
-	INX B ; increase count of number of characters
-	
-	JMP NextCharLoop
+	JZ NextCharLoop
 
 TokenClassEnd:
-	
-	LXI D,TokenList
-	
-LookupToken:
 
-	MOV B,M ; B contains the token value
-					; if we get a match
+	LXI B,TokenList
 	
+	XTHL
+	DAD D
+	
+LookupToken_Loop:
+StrCmp:
 	PUSH H
-	CM Strcmp
-	POP H
-
-	JZ FoundToken
-
-	LDAX D
+	LDAX B
+	INX B
+	XRA M
+	; iff match then A is either 00h or 80h
+	; (80h if last char)
+	INX H
+	JZ Strcmp ; match and not last char
 	
+	; equal to 080h iff match and last char
+	XRI 080h
+	; equal to Z iff match and last char
+
+	POP H
+	
+	JZ Write_Shared
+
+LookupToken:
+	LDAX B
+	MOV D,A
 	INR A
-	INX D
+	INX B
+  JM LookupToken_Loop
   JNZ LookupToken
   
   ; didn't find it, check whether it is a var
 	
-	; If C=1 it might be a var
-	DCR C
+	; If E=2 it might be a var
+	DCR E
+	DCR E
 	JNZ Error
 
-	XTHL 
 	MOV A,M
 	SBI '@'
 	JC Error
 	
-	MOV B,A
-	XTHL
-	
-	; fall through
-	
-FoundToken:
-	XTHL
+	MOV D,A
 	
 	JMP Write_Shared
 	
-; DE points to hi-bit terminated string
 ; HL points to string
+; BC points to string
+; One of them mist be hi-bit terminated
 ; Returns Z set if match
 ; Can't have 128 as a character in D
 ; (so assume that strings must be ASCII)
-Strcmp: 
-	INX D
-	LDAX D
-	CMP M
-	RNZ	; On return Z will be clear
-	
-	CMA
-	ANI 128
-	RZ	; On return Z will be set 
-	
-	INX H
-	JMP Strcmp
 
+; 27 bytes for 6 classes - can it be simplified?
+; bit 6 set : alpha
+; bit 5 and 4 set:
+;  bits3 and (2 or 1): comp
+;  else digit
+; else
+;  check for space and quote
 
-	
+; bits 6,5,4:
+; 000 - not allowed
+; 001
+; 010 LT0, space or quote
+; 011 digit or comp
+; 100 alpha
+;
+; Could be done with 24 bytes using lookup
+; table:
+;
+; ! Space
+; " LT0
+; 35 quote
+; 0 LT0
+; 58 digit
+; 64 comp
+; 255 alpha
+;
+; PUSH H
+; MVI L,LookupClass&0ffh
+; LookupClassLoop:
+; CMP M
+; INR L
+; JNC LookupClassLoop
+; MOV C,M
+; POP H
+
 CharClass:
 ; each character less than 0 is a
 ; distinct class.
 ; 0-9 is a class (class C)
 ; : to > is a class (class 8)
 ; >= @ is a class (class 9)
-	MVI E,FreshStart&0ffh
+	MVI C,FreshStart&0ffh
 	CPI ' '
 	RZ
 	
-	MVI E,QuoteClass&0ffh
+	MVI C,QuoteClass&0ffh
 	CPI 34
 	RZ
 	
-	MVI E,LT0Class&0ffh
+	MVI C,LT0Class&0ffh
 	CPI '0'
 	RC
 
-	MVI E,DigitClass&0ffh
+	MVI C,DigitClass&0ffh
 	CPI '9'+1
 	RC
 
-	MVI E,CompClass&0ffh
+	MVI C,CompClass&0ffh
 	CPI '@'
 	RC
 
-	INR E
+	INR C
 	RET
 
 Error:
@@ -259,7 +285,7 @@ Error:
 org 0300h
 
 TokenList:
-	DB PrintSub&0ffh
+	DB (PrintSub&0ffh)
 	DB "PRIN",'T'+128
 	DB 255
 	
